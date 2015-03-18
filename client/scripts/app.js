124b69ed3277b7051e0cc119d9c9a66511dbcedd
MKS-Student <mks.student@hackreactor.com>
2015-03-18 16:15:43 -0500

    More work towards implementing chatterbox client - init & fetch.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,6 +1,8 @@
 // YOUR CODE HERE:
 var app = {
-	init: function(){},
+	init: function(){
+		this.fetch();
+	},
 	send: function(){
 		$.ajax({
 		  // This is the url you should use to communicate with the parse API server.
@@ -16,5 +18,36 @@ var app = {
 		    console.error('chatterbox: Failed to send message');
 		  }
 		});
-	}
+	},
+	fetch: function(){
+		$.ajax({
+		  // This is the url you should use to communicate with the parse API server.
+		  url: 'https://api.parse.com/1/classes/chatterbox',
+		  type: 'GET',
+		  //data: JSON.parse(message),
+		  contentType: 'application/json',
+		  success: function (data) {
+		    console.log('chatterbox: Message fetched');
+		    console.log(data);
+		    for (var i = 0; i < data.results.length; i++){
+		    	console.log(data);
+		    	var messageString = "<div class='message'>Name: "+ data.results[i].username + " : " + data.results[i].text + "</div>"
+		    	$("#messages").append(messageString);
+		    };
+		  },
+		  error: function (data) {
+		    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+		    console.error('chatterbox: Failed to fetch message');
+		  }
+		});
+	},
+	clearMessages: function(){},
+
+	addMessage: function(){},
+
+	addRoom: function(){},
+
+	addFriend: function(){},
+
+	handleSubmit: function(){}
 };
